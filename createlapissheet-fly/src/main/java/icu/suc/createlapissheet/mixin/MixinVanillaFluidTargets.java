7d40daa6b654b6e9791b7ae75e53fc9aa7cf5b8a
KKW557 <kevin557557@gmail.com>
2026-04-04 19:46:31 +0800

    p

---
+++ createlapissheet-fly/src/main/java/icu/suc/createlapissheet/mixin/MixinVanillaFluidTargets.java
@@ -0,0 +1,30 @@
+package icu.suc.createlapissheet.mixin;
+
+import com.zurrtum.create.AllFluids;
+import com.zurrtum.create.content.fluids.pipes.VanillaFluidTargets;
+import com.zurrtum.create.infrastructure.fluids.BottleFluidInventory;
+import com.zurrtum.create.infrastructure.fluids.FluidStack;
+import icu.suc.createlapissheet.Fluids;
+import icu.suc.createlapissheet.content.fluids.pipes.ExperienceAttractor;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jspecify.annotations.NonNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(VanillaFluidTargets.class)
+public abstract class MixinVanillaFluidTargets {
+    @Inject(method = "drainBlock", at = @At("HEAD"), cancellable = true)
+    private static void injectDrainBlock(Level level, BlockPos pos, @NonNull BlockState state, boolean simulate, CallbackInfoReturnable<FluidStack> cir) {
+        int xp = state.getValueOrElse(ExperienceAttractor.EXPERIENCE, -1);
+        if (xp > 0) {
+            if (!simulate) {
+                level.setBlockAndUpdate(pos, state.setValue(ExperienceAttractor.EXPERIENCE, 0));
+            }
+            cir.setReturnValue(new FluidStack(Fluids.EXPERIENCE, xp * 1620));
+        }
+    }
+}
